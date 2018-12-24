2f4590396c30224d338a6e3f4fbdd665de255bf3
Donovan Moini <donovanmoini@gmail.com>
2018-12-24 03:53:17 -0800

    Changed all vars to let

--- js/konami.js
+++ js/konami.js
@@ -1,32 +1,32 @@
 // a key map of allowed keys
-var allowedKeys = {
+let allowedKeys = {
     37: "left",
     38: "up",
     39: "right",
     40: "down",
     65: "a",
     66: "b"
-};
+}
 
 // just a little easter egg involving the Konami Code
-var konamiCode = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"];
-var konamiCodePosition = 0;
+let konamiCode = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"]
+let konamiCodePosition = 0
 
 document.addEventListener("keydown", function(e) {
-    var key = allowedKeys[e.keyCode];
-    var requiredKey = konamiCode[konamiCodePosition];
+    let key = allowedKeys[e.keyCode]
+    let requiredKey = konamiCode[konamiCodePosition]
     if (key == requiredKey) {
-        konamiCodePosition++;
+        konamiCodePosition++
         if (konamiCodePosition == konamiCode.length) {
-            activateCheats();
-            konamiCodePosition = 0;
+            activateCheats()
+            konamiCodePosition = 0
         }
     } else {
-        konamiCodePosition = 0;
+        konamiCodePosition = 0
     }
-});
+})
 
 function activateCheats() {
-    var url = "https://www.youtube.com/watch?v=q6EoRBvdVPQ";
-    window.open(url, "_blank");
+    let url = "https://www.youtube.com/watch?v=q6EoRBvdVPQ"
+    window.open(url, "_blank")
 }
